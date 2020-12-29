d18771fab1bac6e476ef03b05f328354425beb46
proiacm <proiacm@gmail.com>
2020-12-29 14:04:38 -0700

    Correct import

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classes from './burgerIngredient.module.css';
+import classes from './BurgerIngredient.module.css';
 import PropTypes from 'prop-types';
 
 const burgerIngredient = (props) => {
