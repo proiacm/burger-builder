079c1d135efdb9a887e7353974cd7ea7a8bd0cf7
proiacm <proiacm@gmail.com>
2020-12-29 16:59:21 -0700

    Import BuildControls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/Aux';
 
@@ -17,7 +18,7 @@ class BurgerBuilder extends Component {
     return (
         <Aux>
           <Burger ingredients={this.state.ingredients} />
-          <div>Build Controls</div>
+          <BuildControls />
         </Aux>
     );
   }
