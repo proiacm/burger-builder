42d54446cfd008077e1608936f8a7dcd78e511d3
proiacm <proiacm@gmail.com>
2020-12-29 17:12:29 -0700

    create addIngredientHandler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,14 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/Aux';
 
+// all caps for global const
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.5,
+  meat: 1.3,
+  bacon: 1
+}
+
 class BurgerBuilder extends Component {
 
   state = {
@@ -11,7 +19,29 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice : 0
+  }
+
+  // receives type of ingredient being added 
+  addIngredientHandler = (type) => {
+    // update to ingredient count
+    const prevCount = this.state.ingredients[type];
+    const updatedCount = prevCount + 1;
+    // state should be update in an immutable way: spread operator distributes props of old ingredient state into new object being created
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    // update to total price
+    const priceAddition = INGREDIENT_PRICES[type];
+    const prevPrice = this.state.totalPrice;
+    const newPrice = prevPrice + priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+  }
+
+  removeIngredientHandler = (type) => {
+
   }
 
   render() {
