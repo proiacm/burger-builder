027cb533d9e94f4d2af460d0c3b1fed67ce9fc28
proiacm <proiacm@gmail.com>
2021-01-04 14:49:58 -0700

    Add price to order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,6 +18,7 @@ const orderSummary = (props) => {
       <ul>
         {ingredientSummary}
       </ul>
+      <p>Total: <strong>$ {props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
       <Button btnType='Danger' clicked={props.cancel}>CANCEL</Button>
       <Button btnType='Success' clicked={props.continue}>CONTINUE</Button>
