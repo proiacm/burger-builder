74233cc85e0d916de1647def498b63420d8b3798
proiacm <proiacm@gmail.com>
2021-01-04 14:44:23 -0700

    build ContinueHandler and pass props

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -86,6 +86,10 @@ class BurgerBuilder extends Component {
     this.setState({purchasing: false});
   };
 
+  purchaseContinueHandler = () => {
+    alert('Confirmed!')
+  };
+
   render() {
 
     // disable button based on value 
@@ -100,7 +104,7 @@ class BurgerBuilder extends Component {
     return (
         <Aux>
           <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-            <OrderSummary ingredients={this.state.ingredients} />
+            <OrderSummary ingredients={this.state.ingredients} cancel={this.purchaseCancelHandler} continue={this.purchaseContinueHandler} />
           </Modal>
           <Burger ingredients={this.state.ingredients} />
           <BuildControls 
