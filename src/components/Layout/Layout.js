5e67ac90e380c8138d2f4aaf4e0fe48177a84a21
proiacm <proiacm@gmail.com>
2021-01-06 11:25:30 -0700

    convert to class comp and add handler

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
 
-const Layout = (props) => {
+class Layout extends Component {
+
+  state = {
+    showSideDrawer: true
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false})
+  };
+
+  render () {
   
     return(
       <Aux>
         <Toolbar />
-        <SideDrawer />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
         <main className={classes.Content}>
-          {props.children}
+          {this.props.children}
         </main>
       </Aux>
-  );
+    );
+  }
 }
 
 export default Layout; 
